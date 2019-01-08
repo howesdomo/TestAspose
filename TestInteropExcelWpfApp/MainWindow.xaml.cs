1aad66b64e32f9272eab0e128e0282d58d07f232
howesdomo <164572429@qq.com>
2019-01-08 17:21:36 +0800

    TestInteropExcelWpfApp(Excel组件打印调试) - 首次提交

---
+++ TestInteropExcelWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestInteropExcelWpfApp
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            initEvent();
+
+        }
+
+        private void initEvent()
+        {
+            this.btnPrint.Click += BtnPrint_Click;
+        }
+
+        private void BtnPrint_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var filePath = System.IO.Path.Combine(Environment.CurrentDirectory, "广汽确认单打印.xlsx");
+                using (var excelApp = new Util.Excel.ExcelApp_V2())
+                {
+                    excelApp.Open(filePath);
+                    string workSheetName = excelApp.WorksheetName;
+                    string msg = "{0}".FormatWith(workSheetName);
+                    System.Diagnostics.Debug.WriteLine(msg);
+
+
+                    excelApp.Print(isLandscape: true);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.GetFullInfo());
+            }
+
+            string msg2 = "{0}".FormatWith("方法运行结束");
+            System.Diagnostics.Debug.WriteLine(msg2);
+        }
+    }
+}
